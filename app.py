f8b100bce78a9b88cdb2b382395c524e5662f2bb
Moni Sallam <monisallam@gmail.com>
2022-10-04 16:27:49 +0000

    new app

--- app.py
+++ app.py
@@ -10,11 +10,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        data_url = request.form["data_url"]
         response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            model="code-davinci-002",
+            prompt=generate_prompt(data_url),
+            temperature=0,
+            max_tokens=300,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +26,7 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(data_url):
+    return "\"\"\"\nfetch csv file at url '{}' and read into DataFrame\n\"\"\"".format(
+        data_url
     )
